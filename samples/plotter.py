e3bbcb0cb3f420a15d690ebc6d9e0472eaeffde1
Nitish Kumar Kasaraguppe Veerappa Gowda <kasaraguppe@lx3agpu1.physik.rwth-aachen.de>
2024-06-11 14:01:55 +0200

    Update plotter

--- samples/plotter.py
+++ samples/plotter.py
@@ -1,118 +1,83 @@
 import numpy as np
+import awkward as ak
 import matplotlib.pyplot as plt
-import mplhep as hep
+import mplhep as hep # this is a package that helps to plot in CMS style. mplhep = MatPlotLib for High Energy Physics
 from pathlib import Path
 import argparse
 
-from load_samples import load_parquet
 
 def load_parquet_files(NTuples_path):
 
-    # load the data parquet files and select the events in VBF eneriched region
-    data_array = select_events_VBF_enriched(f'{NTuples_path}/DataC_2022/nominal')
-    print(f'INFO: Loaded and selected VBF enriched events from {NTuples_path}/DataC_2022/nominal')
-
     # List of MC samples
-    MC_samples = ["ggh", "vbf", "vh", "tth"]
-    MC_array_dict = {}
+    MC_samples = ["GGJets", "GJetPt40", "GluGluHToGG", "VBFHToGG", "VHToGG", "ttHToGG", "GluGluToHH"]
+    MC_array_dict = {}  # create empty dictionary in which you can add the different samples loaded
+    # you can read more about the dictionaries in python
 
     for MC in MC_samples:
-        # load the MC parquet files and select the events in VBF eneriched region
-        MC_array_dict[MC] = select_events_VBF_enriched(f'{NTuples_path}/{MC}_M-125_preEE/nominal')
-        print(f'INFO: Loaded and selected VBF enriched events from {NTuples_path}/{MC}_M-125_preEE/nominal')
+        # load the MC parquet files and add them to the MC_array_dict
+        MC_array_dict[MC] = ak.from_parquet(f'{NTuples_path}/{MC}/nominal')
+        print(f'INFO: Loaded and selected VBF enriched events from {NTuples_path}/{MC}/nominal')
 
-    return data_array, MC_array_dict
+    return MC_array_dict
 
-def plot_variables(config_file, NTuples_path, outpath):
 
+def plot_variables(config, NTuples_path, outpath):
 
     # get data and MC arrays
-    data_arr, MC_arr = load_parquet_files(NTuples_path)
-
+    MC_arr = load_parquet_files(NTuples_path)
 
     # get the histograms and plot it
-    for var in vars_config.keys():
-        plt.style.use(hep.style.CMS)
+    for var in config.keys():
+        var_config = config[var]
+        plt.style.use(hep.style.CMS) # this is to set the style of the plotting to be of CMS style. You do not have to worry about it much for now
 
-        config = vars_config[var]
-        binning = np.linspace(config["hist_range"][0], config["hist_range"][1], config["n_bins"] + 1)
-        
-        #data_hist, data_edges = np.histogram(data_arr[config["name"]], bins=binning)
-        #data_hist_sum = np.sum(data_hist)
+        binning = np.linspace(var_config["hist_range"][0], var_config["hist_range"][1], var_config["n_bins"] + 1)
 
         MC_hists = {}
         MC_edges = {}
         for MC in MC_arr.keys():
-            weight = np.asarray(MC_arr[MC].weight)
-            MC_hists[MC], MC_edges[MC] = np.histogram(MC_arr[MC][config["name"]], bins=binning, weights=weight)
+            weight = np.asarray(MC_arr[MC].weight) # get the weights for each event
+            MC_hists[MC], MC_edges[MC] = np.histogram(MC_arr[MC][var_config["name"]], bins=binning, weights=weight, density=True)
+            # check more about the np.histogram and its arguments
 
             hep.histplot((MC_hists[MC], MC_edges[MC]), histtype='step', label=MC)
-        
-        hep.histplot((data_hist, data_edges), histtype='errorbar', yerr=np.sqrt(data_hist), label="Data", color="black")
 
         # set other plotting configs
-        plt.xlabel(config['xlabel'])
-        plt.ylabel(config['ylabel'])
+        plt.xlabel(var_config['xlabel'])
+        plt.ylabel(var_config['ylabel'])
         plt.yscale("log")
         plt.legend()
-        plt.xlim(config['plot_range'])
+        plt.xlim(var_config['plot_range'])
         plt.ylim(bottom=0)
-        hep.cms.label("Private work", data=True, year="2022", com=13.6)
+        hep.cms.label("Private work", data=False, year="2022", com=13.6)
 
         # save the histograms
-        Path(outpath).mkdir(exist_ok=True)
+        Path(outpath).mkdir(exist_ok=True) # this creates a directory in the mentioned 'outpath'. Similar to mkdir you use in terminal
 
-        plt.savefig(f"{outpath}/{var}_plot.png")
+        plt.savefig(f"{outpath}/{var}_plot.png") # saves the figure as png
         print(f"INFO: Histogram saved for {var} in {outpath}")
         plt.clf()
 
-var_dict['diphoton_pt']['name']
+# define the variable to plot
 var_dict = {
     "diphoton_pt": {
-    "name": "pt",
-    "xlabel": "$\\mathrm{p}_{T}^{\\gamma\\gamma}$ [GeV]",
-    "ylabel": "Events per bin",
-    "hist_range": [0, 300],
-    "n_bins": 10,
-    "plot_range": [0, 300]
-    },
-    "second_jet_pt": {
-        "name": "second_jet_pt",
-        "xlabel": "$p_{T}^{j_2}$ [GeV]",
+        "name": "pt", # this is the variable name in the samples
+        "xlabel": "$\\mathrm{p}_{T}^{\\gamma\\gamma}$ [GeV]",
         "ylabel": "Events per bin",
-        "hist_range": [0, 250],
-        "n_bins": 10,
-        "plot_range": [0, 250]
+        "hist_range": [0, 500],
+        "n_bins": 40,
+        "plot_range": [0, 500]
     },
-    "dijet_delta_phi": {
-        "name": "dijet_delta_phi",
-        "xlabel": "$|\\Delta\\phi_{j_1,j_2}|$",
+    "diphoton_mass": {
+        "name": "mass",
+        "xlabel": "$m_{j_1j_2}$ [GeV]",
         "ylabel": "Events per bin",
-        "hist_range": [0, 3.142],
-        "n_bins": 10,
-        "plot_range": [0, 3.142]
-    },
-    "dijet_mass": {
-        "name": "dijet_mass",
-        "xlabel": "$m_{j_1j_2} [GeV]$",
-        "ylabel": "Events per bin",
-        "hist_range": [200, 1000],
-        "n_bins": 10,
-        "plot_range": [200, 1000]
+        "hist_range": [100, 200],
+        "n_bins": 40,
+        "plot_range": [100, 200]
     }
 }
 
-
-if __name__ == "__main__":
-    # Set up argument parser for command line arguments
-    parser = argparse.ArgumentParser(description="Plotting script")
-    parser.add_argument('config_file', type=str, help="Path to the configuration file")
-    parser.add_argument('input_dir', type=str, help="Directory containing the input samples")
-    parser.add_argument('output_dir', type=str, help="Directory to save the output plots")
-
-    # Parse the command line arguments
-    args = parser.parse_args()
-
-    plot_variables(args.config_file, args.input_dir, args.output_dir)
-
-
+# call the plotting function
+samples_path = "/net/scratch_cms3a/kasaraguppe/public/HHbbgg_samples/"
+plot_variables(var_dict, samples_path, "plots")
