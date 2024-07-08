804b794a1231e62eed504adb5f241d9a299c30ed
Nitish Kumar Kasaraguppe Veerappa Gowda <kasaraguppe@lx3agpu1.physik.rwth-aachen.de>
2024-07-08 11:06:41 +0200

    Add basic mlp and a script for training utils

---
+++ models/training_utils.py
@@ -0,0 +1,7 @@
+from mlp import MLP
+
+# the MLP model is already imported
+# define all other things that are required for training:
+# take the inputs, apply standardization, separate and randomize inputs into training/test sets, optimization function, loss function, backpropagation and weight updation, etc.
+
+
